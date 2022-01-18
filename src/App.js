ad1d69d590941ac711027dcc89e2eef5e8e20818
Casey Carroll <caseydean8@gmail.com>
2022-01-18 11:37:24 -0500

    Added founders component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
+import Founders from './components/Founders';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Carousel />
+      <Founders />
     </div>
   );
 }
---
+++ src/components/Founders.js
@@ -0,0 +1,12 @@
+import React from "react";
+import foundersImg from "../assets/founders.jpg";
+
+const Founders = () => {
+  return (
+    <div className="founders">
+      <img src={foundersImg} alt="founders" />;
+    </div>
+  );
+};
+
+export default Founders;
