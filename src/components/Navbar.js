5104181f38a8f832996e6939e5ac4ded4194dc1b
Casey Carroll <caseydean8@gmail.com>
2022-01-18 10:13:14 -0500

    Added Carousel component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,42 +29,37 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">
-                Link
+              <Link className="nav-link" to="/the-whiskey">
+                THE WHISKEY
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">
-                Link
+              <Link className="nav-link" to="/our-story">
+                OUR STORY
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">
-                Link
+              <Link className="nav-link" to="/cocktails">
+               COCKTAILS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/recogition">
+                RECOGNITION
+              </Link>
+            </li>{" "}
+            <li className="nav-item">
+              <Link className="nav-link" to="/news">
+               NEWS 
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/buy-now">
+               BUY NOW 
               </Link>
             </li>
           </ul>
-          {/* <Button>Portfolio</Button> */}
-          {/* <button type="button" class="btn btn-light">Light</button> */}
-          {/* <Button variant="light">Light</Button> */}
-
-          {/* <Button /> */}
-          {/* <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form> */}
         </div>
-        {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
       </div>
     </nav>
   );
