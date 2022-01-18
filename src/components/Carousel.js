7c85cd8300303bf3fd28af864c18abbf9af25ddf
Casey Carroll <caseydean8@gmail.com>
2022-01-18 10:38:51 -0500

    Imported carousel images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,29 +1,54 @@
 import React from "react";
+import spirit from "../assets/spirit.png";
+import ryeDelivered from "../assets/rye-delivered.jpg";
+import farmTo from "../assets/farm-to-bottle.jpg";
 
 const Carousel = () => {
   return (
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."/>
+    <div
+      id="carouselExampleControls"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={spirit} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={ryeDelivered}
+            className="d-block w-100"
+            alt="..."
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={farmTo}
+            className="d-block w-100"
+            alt="..."
+          />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-  )
-}
+  );
+};
 
 export default Carousel;
