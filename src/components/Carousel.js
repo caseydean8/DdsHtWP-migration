3b600bc221af0eea16f6bcbb66b58d0d2663a3d1
Casey Carroll <caseydean8@gmail.com>
2022-02-08 10:19:03 -0500

    Added media queries for improved carousel view.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,56 +1,50 @@
 import React from "react";
 import spirit from "../assets/spirit.png";
 import ryeDelivered from "../assets/rye-delivered.jpg";
-import farmTo from "../assets/farm-to-bottle.jpg";
-import CarouselCard from "./CarouselCard";
+import farmTo from "../assets/wheat-field-dh.jpeg";
+// import CarouselCard from "./CarouselCard";
 
 const Carousel = () => {
   return (
     // <div className="row">
-      <div
-        id="carouselExampleControls"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={spirit} className="d-block w-100" alt="..." />
-            <CarouselCard />
-          </div>
-          <div className="carousel-item">
-            <img src={ryeDelivered} className="d-block w-100" alt="..." />
-            <CarouselCard />
-          </div>
-          <div className="carousel-item">
-            <img src={farmTo} className="d-block w-100" alt="..." />
-            <CarouselCard />
-          </div>
+    <div
+      id="carouselExampleControls"
+      className="carousel slide carousel-fade"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={spirit} id="spirit" className="d-block car-img" alt="..." />
+          {/* <CarouselCard buttonText={"Spirit of Tradition"} /> */}
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        <div className="carousel-item">
+          <img src={ryeDelivered} id="rye-delivered" className="d-block car-img" alt="..." />
+          {/* <CarouselCard buttonText={"Rye Delivered"} /> */}
+        </div>
+        <div className="carousel-item">
+          <img src={farmTo} className="d-block car-img" alt="..." />
+          {/* <CarouselCard buttonText={"Farm To Bottle"} /> */}
+        </div>
       </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
     // </div>
   );
 };
--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -1,23 +1,24 @@
 import React from "react";
 
-const CarouselCard = () => {
+const CarouselCard = (props) => {
   return (
-<div className="carousel-caption">
-    <div className="card text-center">
-  <div className="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-  <div className="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
-</div>
-  )
-}
+    <div className="carousel-caption">
+      <div className=" animated fadeIn card text-center">
+        <div className="card-header">Featured</div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            {props.buttonText}
+          </a>
+        </div>
+        <div className="card-footer text-muted">2 days ago</div>
+      </div>
+    </div>
+  );
+};
 
 export default CarouselCard;
