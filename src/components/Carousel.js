b564b8519a61fd624c39447d2e3da43ec69af189
Casey Carroll <caseydean8@gmail.com>
2022-02-09 11:32:23 -0500

    Added captions to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -16,14 +16,52 @@ const Carousel = () => {
         <div className="carousel-item active">
           <img src={spirit} id="spirit" className="d-block car-img" alt="..." />
           {/* <CarouselCard buttonText={"Spirit of Tradition"} /> */}
+          <div className="carousel-caption d-md-block">
+            <div className="bold">
+              <h1>SPIRIT</h1>
+            </div>
+            <div className="cursive">
+              <h1>of Tradition</h1>
+            </div>
+            <p>
+              Dad's Hat Rye taps into the Keystone State's agricultural richness
+              to produce genuine rye right here in Bucks County.
+            </p>
+          </div>
         </div>
         <div className="carousel-item">
-          <img src={ryeDelivered} id="rye-delivered" className="d-block car-img" alt="..." />
+          <img
+            src={ryeDelivered}
+            id="rye-delivered"
+            className="d-block car-img"
+            alt="..."
+          />
           {/* <CarouselCard buttonText={"Rye Delivered"} /> */}
+          <div className="carousel-caption d-md-block r-d-label">
+            <div className="bold">
+              <h1>Rye</h1>
+            </div>
+            <div className="cursive">
+              <h1>Delivered</h1>
+            </div>
+            <p>
+              Dad's Hat is real rye whiskey made the right way. Now Pennsylvania
+              residents can order online for direct delivery
+            </p>
+          </div>
         </div>
         <div className="carousel-item">
           <img src={farmTo} className="d-block car-img" alt="..." />
           {/* <CarouselCard buttonText={"Farm To Bottle"} /> */}
+          <div className="carousel-caption d-md-block r-d-label">
+            <div className="bold">
+              <h1>FARM</h1>
+            </div>
+            <div className="cursive">
+              <h1>To Bottle</h1>
+            </div>
+            <p>The process begins with rye grain grown by local farms - the most vital ingredient when making genuine rye whiskey</p>
+          </div>
         </div>
       </div>
       <button
