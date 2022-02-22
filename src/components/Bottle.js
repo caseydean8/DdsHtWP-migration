b507e1604e4e3b8fe8107701166ec9fe0cd7bbb1
Casey Carroll <caseydean8@gmail.com>
2022-02-22 09:41:00 -0500

    Added modals to Bottles.

--- src/components/Bottle.js
+++ src/components/Bottle.js
@@ -1,19 +1,31 @@
 import React from "react";
 // imr: import React from 'react'
 import Keystone from "../assets/keystone-divider.png";
+import BottleModal from './BottleModal';
 
 const Bottle = (props) => {
   return (
-    <div className="bottle-wrapper">
-      <div className="bottle">
-        <img className="bottle-img" src={props.image} alt={props.alt} />
+    <div className="modal-wrapper">
+      <div className="bottle-wrapper">
+        <div className="bottle">
+          <img className="bottle-img" src={props.image} alt={props.alt} />
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary bottle-modal"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          {props.flavor}
+        </button>
+        <div className="keystone-divider">
+          <img className="keystone" src={Keystone} alt="keystone divider" />
+          <p className="keystone-text">{props.flavor}</p>
+        </div>
+        <hr className="bottle-divider"></hr>
+        <hr id="bd-2" className="bottle-divider"></hr>
       </div>
-      <div className="keystone-divider">
-        <img className="keystone" src={Keystone} alt="keystone divider" />
-        <p className="keystone-text">{props.flavor}</p>
-      </div>
-      <hr className="bottle-divider"></hr>
-      <hr id="bd-2" className="bottle-divider"></hr>
+      <BottleModal flavor={props.flavor} />
     </div>
   );
 };
